5c295a65224036552c46c50c6b2a43271f480536
jninus <reda.zejli@gmail.com>
2019-11-16 01:05:58 +0100

    Avancement

--- src/components/widgets/NewsList/newsList.js
+++ src/components/widgets/NewsList/newsList.js
@@ -5,10 +5,12 @@ import axios from 'axios'
 import { URL } from '../../../config'
 import styles from './newsList.css'
 import Button from '../Buttons/buttons'
+import CardInfo from '../CardInfo/cardInfo'
 
 class NewsList extends Component {
 
     state = {
+        teams: [],
         items: [],
         start: this.props.start,
         end: this.props.start + this.props.amount,
@@ -20,6 +22,17 @@ class NewsList extends Component {
     }
 
     request = (start, end) => {
+
+        if (this.state.teams.length < 1) {
+            axios.get(`${URL}/teams`).then((response) => {
+
+                this.setState({
+                    teams: response.data
+                })
+
+            })
+        }
+
         axios.get(`${URL}/articles?_start=${start}&_end=${end}`).then((response) => {
             this.setState({
                 items: [...this.state.items, ...response.data]
@@ -50,6 +63,7 @@ class NewsList extends Component {
                             <div>
                                 <div className={styles.newslist_item}>
                                     <Link to={`articles/${item.id}`}>
+                                        <CardInfo teams={this.state.teams} team={item.team} date={item.date} />
                                         <h2>{item.title}</h2>
                                     </Link>
 
@@ -69,6 +83,7 @@ class NewsList extends Component {
 
 
     render() {
+
         return (
             <div>
                 <TransitionGroup
